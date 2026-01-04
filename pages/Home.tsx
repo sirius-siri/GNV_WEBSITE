6f56d2a5ae0fa5e27b7c47384e6ecbd2b0341e32
sirius-siri <shreesathiyavel@gmail.com>
2026-01-04 23:32:40 +0530

    Updated home.tsx

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -12,10 +12,7 @@ const Home: React.FC = () => {
         {/* Background Image with Overlay */}
         <div className="absolute inset-0 z-0">
           {/* <img 
-            src="https://images.unsplash.com/photo-1523050854058-8df90110c9f1?auto=format&fit=crop&q=80&w=1920" 
-            alt="Students graduating" 
-            className="w-full h-full object-cover"
-          /> */}
+          
           {/* Light Overlay: White/Pink Fade */}
           <div className="absolute inset-0 bg-gradient-to-t from-[#FFF0F0] via-[#FFF0F0]/60 to-[#FF6B6B]/20" />
         </div>
